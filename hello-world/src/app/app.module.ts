3bad18f42d6fe199797c90c26450af0e3f7f23e5
Paul Rail <paul.a.rail@gmail.com>
2017-11-04 22:47:04 -0400

    Continuing with reusable components

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,19 +1,16 @@
-import { TitleCasePipe } from './summary.pipe';
-import { CoursesComponent } from './courses.component';
+
+import { FavoriteComponent } from './favorite/favorite.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CourseComponent } from './course/course.component';
-import { AuthorsComponent } from './authors/authors.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent,
-    // SummaryPipe,
-    TitleCasePipe
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,28 +1,13 @@
-// import { CoursesService } from './courses.service';
-
 import { Component } from '@angular/core';
-import {TitleCasePipe} from './summary.pipe';
 
-// declerator function:
 @Component({
-
-  selector: 'isFavorite',
-
-  template: `
-    <input [(ngModel)]="usermessage" (keydown)="onKeyPress()" />
-    <br/>
-    {{ text | titleCase }}
-  `
-  // <h2 id="Proper"></h2>
-  })
-
-  export class CoursesComponent {
-    usermessage: string;
-    text = '';
-
-    onKeyPress() {
-      console.log(this.usermessage);
-      // document.getElementById("Proper").innerHTML = this.usermessage;
-      this.text = this.usermessage;
-    }
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  post = {
+    title: 'Title',
+    isFavorite: true
   }
+}
